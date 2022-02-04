c3bb294dca937f6199776c32aabec96e9ede119c
katataku <katataku.dev@gmail.com>
2022-02-04 19:04:48 +0900

    feat:複数のT_WORDを返す処理を追加

---
+++ srcs/parse/parse_token.c
@@ -0,0 +1,26 @@
+#include "minishell.h"
+#include "typedefs.h"
+#include "xlibft.h"
+
+t_token	*parse_token(const char *str)
+{
+	t_token	*ans;
+	int		i;
+	char	**strs;
+
+	ans = (t_token *)ft_xcalloc(sizeof(t_token), 1);
+	strs = ft_split(str, ' ');
+	i = 0;
+	while (strs[i] != NULL)
+		i++;
+	ans->token = (int *)ft_xcalloc(sizeof(int), i + 1);
+	ans->word = (char **)ft_xcalloc(sizeof(char *), i + 1);
+	i = 0;
+	while (strs[i] != NULL)
+	{
+		ans->token[i] = T_WORD;
+		ans->word[i] = ft_xstrdup(strs[i]);
+		i++;
+	}
+	return (ans);
+}
--- tests/google_tests/main.cpp
+++ tests/google_tests/main.cpp
@@ -20,5 +20,20 @@ TEST(parse_token, word_1)
 	token = parse_token(word);
 	ASSERT_EQ(token->token[0], T_WORD);
 	ASSERT_STREQ(token->word[0], word);
+	ASSERT_EQ(token->token[1], NULL);
+	ASSERT_STREQ(token->word[1], NULL);
+}
+
+TEST(parse_token, word_2_space)
+{
+	t_token *token;
+	char *word = "ls -la";
 	
+	token = parse_token(word);
+	ASSERT_EQ(token->token[0], T_WORD);
+	ASSERT_STREQ(token->word[0], "ls");
+	ASSERT_EQ(token->token[1], T_WORD);
+	ASSERT_STREQ(token->word[1], "-la");
+	ASSERT_EQ(token->token[2], NULL);
+	ASSERT_STREQ(token->word[2], NULL);
 }
