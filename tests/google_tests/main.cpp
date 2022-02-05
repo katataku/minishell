8a130ae012d5f455c140d5b21a8cd7b28ce14304
katataku <katataku.dev@gmail.com>
2022-02-05 15:26:59 +0900

    fix:refactor test 共通処理を関数化

--- tests/google_tests/main.cpp
+++ tests/google_tests/main.cpp
@@ -7,6 +7,18 @@ extern "C" {
 #include "typedefs.h"
 }
 
+void	check_token_assert(char *input, int token_len, int *expected_token, char **expected_word)
+{
+	t_token *token;
+	token = parse_token(input);
+
+	for (int i = 0; i < token_len; i++)
+	{
+		ASSERT_EQ(token->token[i], expected_token[i]);
+		ASSERT_STREQ(token->word[i], expected_word[i]);
+	}
+}
+
 TEST(minishell, normal_case)
 {
     ASSERT_EQ(0, 0);
@@ -14,42 +26,32 @@ TEST(minishell, normal_case)
 
 TEST(parse_token, word_1)
 {
-	t_token *token;
-	char *word = "hoge";
-	
-	token = parse_token(word);
-	ASSERT_EQ(token->token[0], T_WORD);
-	ASSERT_STREQ(token->word[0], word);
-	ASSERT_EQ(token->token[1], NULL);
-	ASSERT_STREQ(token->word[1], NULL);
+	char *input = "hoge";
+	int token_len = 2;
+	int expected_token[] = {T_WORD, NULL};
+	char *expected_word[] = {"hoge", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+
 }
 
 TEST(parse_token, word_2_space)
 {
-	t_token *token;
-	char *word = "ls -la";
-	
-	token = parse_token(word);
-	ASSERT_EQ(token->token[0], T_WORD);
-	ASSERT_STREQ(token->word[0], "ls");
-	ASSERT_EQ(token->token[1], T_WORD);
-	ASSERT_STREQ(token->word[1], "-la");
-	ASSERT_EQ(token->token[2], NULL);
-	ASSERT_STREQ(token->word[2], NULL);
+	char *input = "ls -la";
+	int token_len = 3;
+	int expected_token[] = {T_WORD, T_WORD, NULL};
+	char *expected_word[] = {"ls", "-la", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
 }
 
 TEST(parse_token, bar_1_normal)
 {
-	t_token *token;
-	char *word = "ls | cat";
-	
-	token = parse_token(word);
-	ASSERT_EQ(token->token[0], T_WORD);
-	ASSERT_STREQ(token->word[0], "ls");
-	ASSERT_EQ(token->token[1], T_BAR);
-	ASSERT_STREQ(token->word[1], NULL);
-	ASSERT_EQ(token->token[2], T_WORD);
-	ASSERT_STREQ(token->word[2], "cat");
-	ASSERT_EQ(token->token[3], NULL);
-	ASSERT_STREQ(token->word[3], NULL);
+
+	char *input = "ls | cat";
+	int token_len = 3;
+	int expected_token[] = {T_WORD, T_BAR, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "cat", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
 }
