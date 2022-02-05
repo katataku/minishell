e91b654987a79fae83380c81fd457b94c5675a09
katataku <katataku.dev@gmail.com>
2022-02-05 16:16:07 +0900

    fix:refactor tokenize 共通処理の関数化

--- srcs/parse/tokenize.c
+++ srcs/parse/tokenize.c
@@ -2,6 +2,24 @@
 #include "typedefs.h"
 #include "xlibft.h"
 
+int	count_token_num(char *str)
+{
+	size_t	i;
+	int		ans;
+
+	i = 0;
+	ans = 1;
+	while (str[i] != '\0')
+	{
+		if (str[i] == ' ')
+			ans += 1;
+		if (str[i] == '|')
+			ans += 2;
+		i++;
+	}
+	return (ans + 1);
+}
+
 t_token	*tokenize(const char *str)
 {
 	t_token	*ans;
@@ -9,12 +27,9 @@ t_token	*tokenize(const char *str)
 	char	**strs;
 
 	ans = (t_token *)ft_xcalloc(sizeof(t_token), 1);
+	ans->token = (int *)ft_xcalloc(sizeof(int), count_token_num(str));
+	ans->word = (char **)ft_xcalloc(sizeof(char *), count_token_num(str));
 	strs = ft_split(str, ' ');
-	i = 0;
-	while (strs[i] != NULL)
-		i++;
-	ans->token = (int *)ft_xcalloc(sizeof(int), i + 1);
-	ans->word = (char **)ft_xcalloc(sizeof(char *), i + 1);
 	i = 0;
 	while (strs[i] != NULL)
 	{
