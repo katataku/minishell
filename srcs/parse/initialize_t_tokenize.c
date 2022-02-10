2b080c14cbc464e1d24500a9319285706fe72512
katataku <katataku.dev@gmail.com>
2022-02-10 10:16:59 +0900

    fix:commit漏れ

---
+++ srcs/parse/initialize_t_tokenize.c
@@ -0,0 +1,35 @@
+#include "minishell.h"
+#include "typedefs.h"
+#include "xlibft.h"
+
+int	count_token_num(char *str)
+{
+	size_t	i;
+	int		token_num;
+
+	i = 0;
+	token_num = 1;
+	while (str[i] != '\0')
+	{
+		if (is_special_char(str[i]))
+			token_num += 2;
+		if (str[i] == ' ')
+			token_num -= 1;
+		if (str[i] == '>' && str[i + 1] == '>')
+			i++;
+		if (str[i] == '<' && str[i + 1] == '<')
+			i++;
+		i++;
+	}
+	return (token_num + 1);
+}
+
+t_token	*initialize_t_token(char *str)
+{
+	t_token	*token;
+
+	token = (t_token *)ft_xcalloc(1, sizeof(t_token));
+	token->token = (int *)ft_xcalloc(count_token_num(str), sizeof(int));
+	token->word = (char **)ft_xcalloc(count_token_num(str), sizeof(char *));
+	return (token);
+}
