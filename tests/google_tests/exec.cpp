5c0df7196d91c7314e255afd4fcf3a27641e7c28
hayashi-ay <go12limchangyong@gmail.com>
2022-02-11 22:16:18 +0900

    fix: テストの処理を共通化

--- tests/google_tests/exec.cpp
+++ tests/google_tests/exec.cpp
@@ -29,12 +29,47 @@ protected:
 		free(exec_info->dstfile);
 		free(exec_info);
 	}
-	void build_cmd(std::string cmds[], int size);
+	void to_exec_info_cmd(std::string cmds[], int size);
+	std::string build_cmd(std::string cmds[], int size);
+	void do_execute(std::string cmds[], int size);
+	void do_execute(std::string cmd);
 };
 
-void ExecTest::build_cmd(std::string cmds[], int size) {
+void ExecTest::to_exec_info_cmd(std::string cmds[], int size) {
 	exec_info->cmds = (char ***)calloc(size, sizeof(char **));
 	exec_info->cmds[0] = ft_split(cmds[0].c_str(), ' ');
+}
+
+std::string ExecTest::build_cmd(std::string *cmds, int size) {
+	std::string cmd;
+
+	cmd = "< ";
+	cmd += exec_info->srcfile;
+	for (int i = 0; i < size; i++)
+	{
+		if (i != 0)
+			cmd += " | ";
+		else
+			cmd += " ";
+		cmd += cmds[i];
+	}
+	cmd += " > expected";
+	return (cmd);
+}
+
+void ExecTest::do_execute(std::string *cmds, int size) {
+	std::string cmd;
+
+	to_exec_info_cmd(cmds, size);
+	cmd = build_cmd(cmds, size);
+	do_execute(cmd);
+}
+
+void ExecTest::do_execute(std::string cmd) {
+	actual_status_code = execute(exec_info);
+	expect_status_code = system(cmd.c_str());
+	ASSERT_EQ(expect_status_code, actual_status_code);
+	ASSERT_EQ(system("diff actual expected"), 0);
 }
 
 TEST_F(ExecTest, single_command)
@@ -42,10 +77,6 @@ TEST_F(ExecTest, single_command)
 	std::string cmds[] = {
 			"/bin/cat"
 	};
-	build_cmd(cmds, 1);
 
-	actual_status_code = execute(exec_info);
-	expect_status_code = system("< infile /bin/cat > expected");
-	ASSERT_EQ(expect_status_code, actual_status_code);
-	ASSERT_EQ(system("diff actual expected"), 0);
+	do_execute(cmds, 1);
 }
