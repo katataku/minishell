90582ebd2dcd1792f90c36753ef10c5b6526f370
Ayato Hayashi <go12limchangyong@gmail.com>
2022-02-11 13:09:56 +0900

    Google Testsを整備 (#25)
    * 空コミット
    
    * fix: CMakeLists.txtを片岡さん版に合わせる
    
    * feat: Google Testにmain関数を追加
    
    * feat: 実行部分のテストを箱だけ用意
    
    * chore: テストを分離
    
    * fix: dollarがtypeしていたのでついでに修正
    
    * chore: normのバージョンも表示するように
    
    * fix: ymlのsyntaxエラー修正

---
+++ tests/google_tests/lexer.cpp
@@ -0,0 +1,278 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "minishell.h"
+}
+
+void	check_token_assert(char *input, int token_len, int *expected_token, char **expected_word)
+{
+	t_token *token;
+	token = lexer(input);
+
+	for (int i = 0; i < token_len; i++)
+	{
+		ASSERT_EQ(token->token[i], expected_token[i]);
+		ASSERT_STREQ(token->word[i], expected_word[i]);
+	}
+}
+
+TEST(lexer, word_1)
+{
+	char *input = "hoge";
+	int token_len = 2;
+	int expected_token[] = {T_WORD, NULL};
+	char *expected_word[] = {"hoge", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+
+}
+
+TEST(lexer, word_2_space)
+{
+	char *input = "ls -la";
+	int token_len = 3;
+	int expected_token[] = {T_WORD, T_WORD, NULL};
+	char *expected_word[] = {"ls", "-la", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, bar_1_normal)
+{
+	char *input = "ls | cat";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_BAR, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "cat", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, bar_1_normal_without_space)
+{
+	char *input = "ls|cat";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_BAR, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "cat", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, semi_1_normal)
+{
+	char *input = "ls ; cat";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_SEMI, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "cat", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, semi_1_normal_without_space)
+{
+	char *input = "ls;cat";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_SEMI, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "cat", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, gt_1_normal)
+{
+	char *input = "ls > outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_GT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, gt_1_normal_without_space)
+{
+	char *input = "ls>outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_GT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, gtgt_1_normal)
+{
+	char *input = "ls >> outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_GTGT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, gtgt_1_normal_without_space)
+{
+	char *input = "ls>>outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_GTGT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, lt_1_normal)
+{
+	char *input = "ls < outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_LT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, lt_1_normal_without_space)
+{
+	char *input = "ls<outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_LT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, ltlt_1_normal)
+{
+	char *input = "ls << outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_LTLT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, ltlt_1_normal_without_space)
+{
+	char *input = "ls<<outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_LTLT, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, sq_1_normal)
+{
+	char *input = "ls ' outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_SQ, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, sq_1_normal_without_space)
+{
+	char *input = "ls'outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_SQ, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, dq_1_normal)
+{
+	char *input = "ls \" outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_DQ, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, dq_1_normal_without_space)
+{
+	char *input = "ls\"outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_DQ, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, bq_1_normal)
+{
+	char *input = "ls ` outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_BQ, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, bq_1_normal_without_space)
+{
+	char *input = "ls`outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_BQ, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, doller_1_normal)
+{
+	char *input = "ls $ outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_DOLLAR, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, doller_1_normal_without_space)
+{
+	char *input = "ls$outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_DOLLAR, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, curly_bracket_open_1_normal)
+{
+	char *input = "ls { outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_C_BRA_OPN, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, curly_bracket_open_1_normal_without_space)
+{
+	char *input = "ls{outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_C_BRA_OPN, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, curly_bracket_close_1_normal)
+{
+	char *input = "ls } outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_C_BRA_CLS, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
+
+TEST(lexer, curly_bracket_close_1_normal_without_space)
+{
+	char *input = "ls}outfile";
+	int token_len = 4;
+	int expected_token[] = {T_WORD, T_C_BRA_CLS, T_WORD, NULL};
+	char *expected_word[] = {"ls", NULL, "outfile", NULL};
+
+	check_token_assert(input, token_len, expected_token, expected_word);
+}
