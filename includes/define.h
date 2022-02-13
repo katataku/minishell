ffb532e4fb535114dbe814fa2576acbcb077018a
hayashi-ay <go12limchangyong@gmail.com>
2022-02-13 16:02:51 +0900

    fix: merge時にlexer関連のtypedefが消えていた

--- includes/define.h
+++ includes/define.h
@@ -54,4 +54,21 @@ typedef struct s_exec_info
 	int		o_flag;
 }	t_exec_info;
 
+typedef struct s_lexer_manager
+{
+	t_token		*token;
+	int			token_index;
+	int			word_index;
+	char		word[4097];
+	int			state;
+}	t_lexer_manager;
+
+enum e_lexer_state
+{
+	NEUTRAL,
+	IN_SQUOTE,
+	IN_DQUOTE,
+	IN_BQUOTE
+};
+
 #endif
