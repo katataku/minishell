e462e12fe6facde14bfa6f62ccb942f477c6b6e1
katataku <katataku.dev@gmail.com>
2022-02-17 20:15:26 +0900

    fix:norm対応

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -13,14 +13,18 @@ int	count_lst(char **lst)
 t_exec_info	*parser(t_token *token)
 {
 	t_exec_info	*exec_info;
+	int			word_cnt;
+	int			i;
 
+	word_cnt = count_lst(token->word);
 	exec_info = (t_exec_info *)calloc(1, sizeof(t_exec_info));
-	exec_info->cmds = (char ***)calloc(2, sizeof(char **));
-	exec_info->cmds[0] = (char **)calloc(3, sizeof(char *));
-
-	for (int i = 0; i < count_lst(token->word); i++)
+	exec_info->cmds = (char ***)calloc(1, sizeof(char **));
+	exec_info->cmds[0] = (char **)calloc(word_cnt, sizeof(char *));
+	i = 0;
+	while (i < word_cnt)
 	{
 		exec_info->cmds[0][i] = token->word[i];
+		i++;
 	}
 	exec_info->cmd_num = 1;
 	exec_info->srcfile = NULL;
