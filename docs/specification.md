3aa248d38463195a479c5df9e28a0ec69e497cd8
Ayato Hayashi <go12limchangyong@gmail.com>
2022-02-18 07:00:44 +0900

    プロンプトからの入力を受け取るようにする (#40)
    * 空コミット
    
    * feat: プロンプト入力を受け付けるように
    
    * chore: 実行ファイルをgitignore
    
    * fix: normの対応およびEOF時に終了するように
    
    * doc: 雑に仕様を追加
    
    * fix: normの対応
    
    * feat: SIGQUITを無視するように
    
    * fix: signalをセットする関数を外に
    
    * feat: interactive時のSIGINTのハンドリング

---
+++ docs/specification.md
@@ -0,0 +1,4 @@
+minishellはEOFで終了する。
+
+SIGQUIT(Ctrl-\)は無視する。
+SIGINT(Ctrl-C)は新しいプロンプトを表示する。
--- main.c
+++ main.c
@@ -1,9 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ahayashi <ahayashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/16 16:09:06 by ahayashi          #+#    #+#             */
+/*   Updated: 2022/02/16 16:09:06 by ahayashi         ###   ########.jp       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
+
+/*
+ * ^Cは表示されてしまう
+ */
+void	sigint_handler(int signal)
+{
+	(void)signal;
+	printf("\n");
+	rl_replace_line("", 0);
+	rl_on_new_line();
+	rl_redisplay();
+	rl_replace_line("", 0);
+}
+
+/*
+ * SIGQUITは特に何もしなくても無視されていそう　
+ */
+void	set_signal(void)
+{
+	signal(SIGQUIT, SIG_IGN);
+	signal(SIGINT, sigint_handler);
+}
 
 int	main(int argc, char **argv, char **env)
 {
+	char	*line;
+
 	(void)argc;
 	(void)argv;
 	(void)env;
-	printf("HELLO WORLD\n");
+	set_signal();
+	while (1)
+	{
+		line = readline("> ");
+		if (line == NULL)
+			break ;
+		if (*line == '\0')
+		{
+			free(line);
+			continue ;
+		}
+		printf("line is '%s'\n", line);
+		add_history(line);
+		free(line);
+	}
 }
