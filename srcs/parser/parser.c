ef0b918653a80ba3769a97e9109a052ca43baf75
katataku <katataku.dev@gmail.com>
2022-02-18 15:38:44 +0900

    fix:norm

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -26,23 +26,32 @@ int	count_cmd_num(int	*lst)
 	return (count);
 }
 
+void	init_parser(t_exec_info **e, t_token *t, int *i, int *j)
+{
+	int	index;
+	int	word_cnt;
+
+	word_cnt = count_lst(t->token);
+	*e = (t_exec_info *)ft_xcalloc(1, sizeof(t_exec_info));
+	(*e)->cmd_num = count_cmd_num(t->token);
+	(*e)->cmds = (char ***)ft_xcalloc((*e)->cmd_num, sizeof(char **));
+	index = 0;
+	while (index < (*e)->cmd_num)
+		(*e)->cmds[index++] = (char **)ft_xcalloc(word_cnt, sizeof(char *));
+	*i = -1;
+	*j = 0;
+}
+
 t_exec_info	*parser(t_token *token)
 {
 	t_exec_info	*exec_info;
-	int			word_cnt;
 	int			idx_token;
 	int			idx_cmd;
 	int			idx_word;
 
-	word_cnt = count_lst(token->token);
-	exec_info = (t_exec_info *)ft_xcalloc(1, sizeof(t_exec_info));
-	exec_info->cmd_num = count_cmd_num(token->token);
-	exec_info->cmds = (char ***)ft_xcalloc(exec_info->cmd_num, sizeof(char **));
-	idx_token = 0;
+	init_parser(&exec_info, token, &idx_token, &idx_cmd);
 	idx_word = 0;
-	idx_cmd = 0;
-	exec_info->cmds[idx_cmd] = (char **)ft_xcalloc(word_cnt, sizeof(char *));
-	while (token->token[idx_token] != NULL)
+	while (token->token[++idx_token] != 0)
 	{
 		if (token->token[idx_token] == T_WORD)
 			exec_info->cmds[idx_cmd][idx_word++] = token->word[idx_token];
@@ -55,10 +64,9 @@ t_exec_info	*parser(t_token *token)
 			exec_info->dstfile = token->word[++idx_token];
 		if (token->token[idx_token] == T_BAR)
 		{
-			exec_info->cmds[++idx_cmd] = (char **)ft_xcalloc(word_cnt, sizeof(char *));
+			idx_cmd++;
 			idx_word = 0;
 		}
-		idx_token++;
 	}
 	return (exec_info);
 }
