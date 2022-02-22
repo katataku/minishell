94947d8008d6c9266c1eb5729e26344bf9fbf7da
hayashi-ay <go12limchangyong@gmail.com>
2022-02-22 17:41:19 +0900

    chore: 出力に色を付ける

--- tests/shell_scripts/main.sh
+++ tests/shell_scripts/main.sh
@@ -1,24 +1,42 @@
 #!/bin/bash
 
+NC="\033[0m"
+BOLD="\033[1m"
+ULINE="\033[4m"
+RED="\033[31m"
+GREEN="\033[32m"
+YELLOW="\033[33m"
+BLUE="\033[34m"
+MAGENTA="\033[35m"
+
 ACTUAL_PATH=./tests/shell_scripts/actual/
 EXPECTED_PATH=./tests/shell_scripts/expected/
 
 # input, test_name
 function do_test() {
+	IS_OK=1
 	echo "${INPUT_CMDS}" | ./minishell > "${ACTUAL_PATH}${TEST_NAME}"
 
-    echo -n "[${TEST_NAME}] status: "
-    if [ $? -eq "${EXPECTED_EXIT_STATUS}" ] ;then
-		echo -n "OK, "
+	echo -n "[${TEST_NAME}] status: "
+	if [ $? -eq "${EXPECTED_EXIT_STATUS}" ] ;then
+		printf "OK, "
 	else
-		echo -n "NG, "
+		printf "${RED}NG${NC}, "
+		IS_OK=0
 	fi
 
-    echo -n "feature: "
-	if  diff "${ACTUAL_PATH}${TEST_NAME}" "${EXPECTED_PATH}${TEST_NAME}"  ;then
-		echo "OK"
+	echo -n "feature: "
+	if  diff "${ACTUAL_PATH}${TEST_NAME}" "${EXPECTED_PATH}${TEST_NAME}" > /dev/null  ;then
+		echo -n "OK"
 	else
-		echo "NG"
+		printf "${RED}NG${NC}"
+		IS_OK=0
+	fi
+
+	if [ $IS_OK -eq 1 ] ; then
+		printf " ${GREEN}[✓]${NC}\n"
+	else
+		printf " ${RED}[-]${NC}\n"
 	fi
 }
 
@@ -33,10 +51,10 @@ EXPECTED_EXIT_STATUS=0
 do_test
 
 # パイプ&パス解決なし
-#TEST_NAME=0002.txt
-#INPUT_CMDS=$(cat << EOS
-#/bin/echo hello2
-#EOS
-#)
-#EXPECTED_EXIT_STATUS=0
-#do_test
+TEST_NAME=0002.txt
+INPUT_CMDS=$(cat << EOS
+/bin/echo hello2
+EOS
+)
+EXPECTED_EXIT_STATUS=0
+do_test
