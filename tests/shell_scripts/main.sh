5813603fd44e65ba3926f2381e65d64d13087976
hayashi-ay <go12limchangyong@gmail.com>
2022-02-24 23:13:46 +0900

    feat: テストを追加

--- tests/shell_scripts/main.sh
+++ tests/shell_scripts/main.sh
@@ -26,7 +26,7 @@ function do_test() {
 	fi
 
 	echo -n "feature: "
-	if  diff "${ACTUAL_PATH}${TEST_NAME}" "${EXPECTED_PATH}${TEST_NAME}" > /dev/null  ;then
+	if  diff "${ACTUAL_PATH}${TEST_NAME}" "${EXPECTED_PATH}${TEST_NAME}" > /dev/null ;then
 		echo -n "OK"
 	else
 		printf "${RED}NG${NC}"
@@ -42,19 +42,36 @@ function do_test() {
 
 # 単一コマンド＆パス解決なし
 TEST_NAME=0001.txt
-INPUT_CMDS=$(cat << EOS
-/bin/echo hello
-EOS
-)
+INPUT_CMDS="/bin/echo hello"
 EXPECTED_EXIT_STATUS=0
-
 do_test
 
 # パイプ&パス解決なし
 TEST_NAME=0002.txt
-INPUT_CMDS=$(cat << EOS
-/bin/echo hello2
-EOS
-)
+INPUT_CMDS="/bin/echo hello | /usr/bin/rev"
+EXPECTED_EXIT_STATUS=0
+do_test
+
+# 単一コマンド＆パス解決なし&infileあり
+TEST_NAME=0003.txt
+INPUT_CMDS="< ./tests/shell_scripts/infile.txt /usr/bin/grep ab"
+EXPECTED_EXIT_STATUS=0
+do_test
+
+# パイプ&パス解決なし&infileあり
+TEST_NAME=0004.txt
+INPUT_CMDS="< ./tests/shell_scripts/infile.txt /usr/bin/grep ab | /usr/bin/wc -c"
+EXPECTED_EXIT_STATUS=0
+do_test
+
+# builtin関数単体
+TEST_NAME=0005.txt
+INPUT_CMDS="echo -n Hello World !"
+EXPECTED_EXIT_STATUS=0
+do_test
+
+# builtin関数withパイプ
+TEST_NAME=0006.txt
+INPUT_CMDS="echo -n Hello World ! | /bin/cat"
 EXPECTED_EXIT_STATUS=0
 do_test
