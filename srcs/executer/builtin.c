29e759fedeee1e05a4255cb562059b6ed5e48222
hayashi-ay <go12limchangyong@gmail.com>
2022-02-24 17:54:20 +0900

    feat: リダイレクトするように

--- srcs/executer/builtin.c
+++ srcs/executer/builtin.c
@@ -12,6 +12,15 @@
 
 #include "executer.h"
 
+static void	replace_fd(int old_fd, int new_fd)
+{
+	if (old_fd != new_fd)
+	{
+		xdup2(old_fd, new_fd);
+		xclose(old_fd);
+	}
+}
+
 bool	is_builtin(char *cmd)
 {
 	return (ft_strcmp("echo", cmd) == 0);
@@ -19,6 +28,15 @@ bool	is_builtin(char *cmd)
 
 int	execute_builtin(int argc, char **argv)
 {
+	int	save_stdout;
+	int	exit_status;
+
+	save_stdout = dup(STDOUT_FILENO);
+	int fd = xopen("actual", O_RDWR | O_CREAT, 0644);
+	replace_fd(fd, STDOUT_FILENO);
+	close(fd);
 	if (ft_strcmp("echo", argv[0]) == 0)
-		return (echo(argc, argv));
+		exit_status = echo(argc, argv);
+	replace_fd(save_stdout, STDOUT_FILENO);
+	return (exit_status);
 }
