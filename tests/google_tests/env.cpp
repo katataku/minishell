48028595191b61c47c49d752abc89c98290f0370
katataku <katataku.dev@gmail.com>
2022-02-24 19:07:46 +0900

    fix:googletest fixtureを使用するように修正

--- tests/google_tests/env.cpp
+++ tests/google_tests/env.cpp
@@ -10,6 +10,9 @@ protected:
 	static void TearDownTestCase() {}
 
 	void SetUp() {
+		set_env("HOME","/Users/takkatao");
+		set_env("PATH","/bin");
+		set_env("PS1","\s-\v\$");
 	}
 	void TearDown() {
 	}
@@ -28,7 +31,6 @@ TEST_F(EnvTest, normal_2)
 	set_env("PATH","/bin");
 	ASSERT_STREQ(get_env("HOME"), "/Users/takkatao");
 	ASSERT_STREQ(get_env("PATH"), "/bin");
-	ASSERT_STREQ(get_env("PS1"), NULL);
 }
 
 TEST_F(EnvTest, update_1)
@@ -40,14 +42,6 @@ TEST_F(EnvTest, update_1)
 
 TEST_F(EnvTest, remove_1)
 {
-	set_env("HOME","/Users/takkatao");
-	set_env("PATH","/bin");
-	set_env("PS1","\s-\v\$");
-
-	ASSERT_STREQ(get_env("HOME"), "/Users/takkatao");
-	ASSERT_STREQ(get_env("PATH"), "/bin");
-	ASSERT_STREQ(get_env("PS1"), "\s-\v\$");
-
 	remove_env("PATH");
 	ASSERT_STREQ(get_env("HOME"), "/Users/takkatao");
 	ASSERT_STREQ(get_env("PATH"), NULL);
