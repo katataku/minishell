ebd37d8a10d2cd7e25ae05fd7e32438ce24e6ac0
hayashi-ay <go12limchangyong@gmail.com>
2022-02-24 15:31:54 +0900

    feat: 複数の出力に対応

--- srcs/builtin/echo.c
+++ srcs/builtin/echo.c
@@ -10,11 +10,27 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdbool.h>
 #include "builtin.h"
 
 int	echo(int argc, char **argv)
 {
-	ft_putstr_fd(argv[1], STDOUT_FILENO);
-	ft_putchar_fd('\n', STDOUT_FILENO);
+	bool	isPrintLineFeed;
+	bool	isFirst;
+
+	isPrintLineFeed = true;
+	isFirst = true;
+	argv++;
+	while (*argv != NULL)
+	{
+		if (!isFirst)
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		else
+			isFirst = false;
+		ft_putstr_fd(*argv, STDOUT_FILENO);
+		argv++;
+	}
+	if (isPrintLineFeed)
+		ft_putchar_fd('\n', STDOUT_FILENO);
 	return (0);
 }
--- tests/google_tests/builtin.cpp
+++ tests/google_tests/builtin.cpp
@@ -15,14 +15,26 @@ protected:
 	void TearDown() {}
 };
 
-TEST_F(BuiltinTest, echo_normal)
+TEST_F(BuiltinTest, echo_one_argument)
 {
 	int	argc = 2;
-	char *argv[] = {"echo", "hello"};
+	char *argv[] = {"echo", "hello", NULL};
 
 	testing::internal::CaptureStdout();
 	status_code = echo(argc, argv);
 	output = testing::internal::GetCapturedStdout();
 	ASSERT_EQ(0, status_code);
 	ASSERT_STREQ("hello\n", output.c_str());
+}
+
+TEST_F(BuiltinTest, echo_multiple_arguments)
+{
+	int	argc = 4;
+	char *argv[] = {"echo", "hello", "world", "!", NULL};
+
+	testing::internal::CaptureStdout();
+	status_code = echo(argc, argv);
+	output = testing::internal::GetCapturedStdout();
+	ASSERT_EQ(0, status_code);
+	ASSERT_STREQ("hello world !\n", output.c_str());
 }
