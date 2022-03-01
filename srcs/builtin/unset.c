faa5f268ba8c4697c08a99cab3767e00c459ac39
kataoka takumi <katataku.dev@gmail.com>
2022-03-01 22:28:57 +0900

    feat:unset追加 (#62)

--- srcs/builtin/unset.c
+++ srcs/builtin/unset.c
@@ -1,25 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin.h                                          :+:      :+:    :+:   */
+/*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/02/24 10:56:18 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/03/01 18:15:15 by takkatao         ###   ########.fr       */
+/*   Created: 2022/02/27 14:20:52 by takkatao          #+#    #+#             */
+/*   Updated: 2022/03/01 18:19:19 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef BUILTIN_H
-# define BUILTIN_H
+#include "builtin.h"
 
-# include "libft.h"
-# include <stdbool.h>
-# include "env.h"
-
-// NOTE: 引数にargcはなくても良いかもしれない。他のbuiltinの様子も見て判断
-int	echo(int argc, char **argv);
-int	env(int argc, char **argv);
-int	unset(int argc, char **argv);
-
-#endif
+/*
+ * usage: unset
+ */
+int	unset(int argc, char **argv)
+{
+	(void)argc;
+	if (argv[1] != NULL)
+		unset_env(argv[1]);
+	return (0);
+}
