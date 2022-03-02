3ed280a919aacd1b611a0e31f30aa59cf3088608
hayashi-ay <go12limchangyong@gmail.com>
2022-03-02 15:59:46 +0900

    doc: update cd docs

--- srcs/builtin/cd.c
+++ srcs/builtin/cd.c
@@ -14,10 +14,13 @@
 
 /*
  * usage: cd [directory]
- * Change the current working directory to the given directory. If directory is
- * not supplied, the value of the HOME shell variable is used. Any additional
- * arguments following directory are ignored. The return status is zero if the
- * directory is successfully changed, non-zero otherwise.
+ * Change the current working directory to given directory. If directory is not
+ * supplied, the value of the HOME shell variable is used. `-` is not supported
+ * as directory. Any additional arguments following directory are ignored.
+ * If `..` appears in directory, it is processed by removing the immediately
+ * preceding pathname component, back to a slash or the beginning of directory.
+ * The return status is zero if the directory is successfully changed, non-zero
+ * otherwise.
  */
 int	cd(int argc, char **argv)
 {
