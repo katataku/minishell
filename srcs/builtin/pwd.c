919e8d2ba6a5655aa24e672c6537ed1f8e2ab8f0
hayashi-ay <go12limchangyong@gmail.com>
2022-03-02 15:03:23 +0900

    feat: implement pwd

--- srcs/builtin/pwd.c
+++ srcs/builtin/pwd.c
@@ -12,9 +12,23 @@
 
 #include "builtin.h"
 
+
+/*
+ * usage: pwd
+ * Print the absolute pathname of the current working directory. The return
+ * status is zero unless an error is encountered while determining the name of
+ * the current directory.
+ */
 int	pwd(int argc, char **argv)
 {
+	char	*pwd;
+
 	(void)argc;
 	(void)argv;
+	pwd = getcwd(NULL, 0);
+	if (pwd == NULL)
+		return (1);
+	ft_putendl_fd(pwd, STDOUT_FILENO);
+	free(pwd);
 	return (0);
 }
