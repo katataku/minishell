7ff8e9efc62b8d87e31f6a00f9fc7c0980b27117
kataoka takumi <katataku.dev@gmail.com>
2022-03-02 11:27:45 +0900

    path展開 (#61)
    * feat:path extraction
    
    * feat:path extractionテスト追加
    
    * fix:マクロ定義に変更
    
    * chore: gitignoreできていなかった
    
    Co-authored-by: hayashi-ay <go12limchangyong@gmail.com>

--- includes/executer.h
+++ includes/executer.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   executer.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahayashi <ahayashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/11 17:10:04 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/02/24 17:09:52 by ahayashi         ###   ########.jp       */
+/*   Updated: 2022/02/27 16:18:14 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,17 @@
 # include "define.h"
 # include "libft.h"
 # include "xsyscall.h"
+# include "env.h"
+# include <sys/types.h>
+# include <unistd.h>
+# include <sys/wait.h>
+# include <sys/errno.h>
+# include <string.h>
 
 int		execute(t_exec_info *exec_info);
 int		execute_builtin(int argc, char **argv);
 int		execute_single_builtin(t_exec_info	*info);
 bool	is_builtin(char *cmd);
+char	*get_fullpath(char *file_name);
 
 #endif
