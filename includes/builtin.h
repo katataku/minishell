988da0eff7610198ece901244dd7e608243d76d6
Ayato Hayashi <go12limchangyong@gmail.com>
2022-03-03 08:36:28 +0900

    builtinのpwdを実装 (#65)
    * 空コミット
    
    * feat: add pwd builtin
    
    * feat: executerからpwdが呼ばれるように
    
    * feat: implement pwd
    
    * fix: norm対応

--- includes/builtin.h
+++ includes/builtin.h
@@ -6,7 +6,7 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 10:56:18 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/03/01 18:15:15 by takkatao         ###   ########.fr       */
+/*   Updated: 2022/03/02 14:31:01 by ahayashi         ###   ########.jp       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,13 @@
 # define BUILTIN_H
 
 # include "libft.h"
-# include <stdbool.h>
 # include "env.h"
+# include "unistd.h"
 
 // NOTE: 引数にargcはなくても良いかもしれない。他のbuiltinの様子も見て判断
 int	echo(int argc, char **argv);
 int	env(int argc, char **argv);
+int	pwd(int argc, char **argv);
 int	unset(int argc, char **argv);
 
 #endif
