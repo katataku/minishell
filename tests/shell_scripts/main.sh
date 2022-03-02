3c9def2e4598b9a81f3abd43b60f3e4fcd70e733
hayashi-ay <go12limchangyong@gmail.com>
2022-03-02 17:20:13 +0900

    feat: cdのテストを追加

--- tests/shell_scripts/main.sh
+++ tests/shell_scripts/main.sh
@@ -112,3 +112,21 @@ echo  -n "> " >> ${EXPECTED_PATH}/${TEST_NAME}
 INPUT_CMDS="env"
 EXPECTED_EXIT_STATUS=0
 do_test
+
+# builtin cd関数(相対パスの場合）
+# 終了ステータスのみを確認
+TEST_NAME=buitlin_cd_relative.txt
+echo "> cd ../../google_tests" > ${EXPECTED_PATH}/${TEST_NAME}
+echo  -n "> " >> ${EXPECTED_PATH}/${TEST_NAME}
+INPUT_CMDS="cd ../../google_tests"
+EXPECTED_EXIT_STATUS=0
+do_test
+
+# builtin cd関数(絶対パスの場合）
+# 終了ステータスのみを確認
+TEST_NAME=buitlin_cd_absolute.txt
+echo "> cd /tmp" > ${EXPECTED_PATH}/${TEST_NAME}
+echo  -n "> " >> ${EXPECTED_PATH}/${TEST_NAME}
+INPUT_CMDS="cd /tmp"
+EXPECTED_EXIT_STATUS=0
+do_test
