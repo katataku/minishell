3a7e48e0b64f806b7f7237848a48b48c0bac6cd8
katataku <katataku.dev@gmail.com>
2022-03-06 00:41:56 +0900

    fix:return statusを設定

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,34 +6,58 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 14:20:52 by takkatao          #+#    #+#             */
-/*   Updated: 2022/03/03 10:11:10 by takkatao         ###   ########.fr       */
+/*   Updated: 2022/03/06 00:41:39 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+// getfullpathにあるputerr_exitといい感じにutili的なところに置きたいかもしれない
+static void	puterr(char *target, char *message)
+{
+	if (ft_putstr_fd(target, STDERR_FILENO) == -1)
+		exit(ERR_CODE_GENERAL);
+	if (ft_putstr_fd(": ", STDERR_FILENO) == -1)
+		exit(ERR_CODE_GENERAL);
+	if (ft_putstr_fd(message, STDERR_FILENO) == -1)
+		exit(ERR_CODE_GENERAL);
+	if (ft_putstr_fd("\n", STDERR_FILENO) == -1)
+		exit(ERR_CODE_GENERAL);
+}
+
 /*
- * usage: unset
+ * usage: export
+ * Mark each name to be passed to child processes in the environment.
+ * If a variable name is followed by =value, the value of the variable is set
+ * to value. In this function, all the environment variables are to be passed
+ * to child process.
+ * The return status is zero unless one of the names is not a valid
+ * shell variable name.
  */
 int	export(int argc, char **argv)
 {
 	char	*key;
 	char	*value;
-	int		i;
+	int		return_status;
 
 	(void)argc;
-	i = 1;
-	while (argv[i] != NULL)
+	return_status = 0;
+	while (*(++argv) != NULL)
 	{
-		key = ft_xstrdup(argv[i]);
+		key = ft_xstrdup(*argv);
 		value = ft_strchr(key, '=');
-		if (value != NULL)
+		if (ft_isalpha(key[0]) || key[0] == '_')
 		{
-			*value = '\0';
-			value++;
-			set_env(key, value);
+			if (value != NULL)
+			{
+				*value = '\0';
+				set_env(key, ++value);
+			}
+			continue ;
 		}
-		i++;
+		return_status = 1;
+		puterr("export", ft_strjoin("`",
+				ft_strjoin(*argv, "`: not a valid identifier")));
 	}
-	return (0);
+	return (return_status);
 }
