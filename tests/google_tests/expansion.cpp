1d52dfdb3c1dbafdd9f0ac87a905ef31f4f56884
hayashi-ay <go12limchangyong@gmail.com>
2022-03-15 11:44:11 +0900

    fix: 箱だけ用意

---
+++ tests/google_tests/expansion.cpp
@@ -0,0 +1,14 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+#include "expansion.h"
+}
+
+class ExpansionTest : public ::testing::Test {
+protected:
+	static void SetUpTestCase() {}
+	static void TearDownTestCase() {}
+
+	void SetUp() {}
+	void TearDown() {}
+};
