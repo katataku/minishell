da5d406e779b90f287a706ec5f26e30042ceb3b7
katataku <katataku.dev@gmail.com>
2022-03-19 13:24:15 +0900

    feat:unsetの複数引数に対応

--- srcs/builtin/unset.c
+++ srcs/builtin/unset.c
@@ -6,7 +6,7 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 14:20:52 by takkatao          #+#    #+#             */
-/*   Updated: 2022/03/07 12:17:01 by ahayashi         ###   ########.jp       */
+/*   Updated: 2022/03/19 13:21:36 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,17 @@
 
 /*
  * usage: unset
+ * Always returns STATUS_SUCCESS.
  */
 int	builtin_unset(char **argv)
 {
-	if (argv[1] != NULL)
-		unset_env(argv[1]);
+	int	i;
+
+	i = 1;
+	while (argv[i] != NULL)
+	{
+		unset_env(argv[i]);
+		i++;
+	}
 	return (STATUS_SUCCESS);
 }
