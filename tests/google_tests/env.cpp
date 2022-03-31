5055d6059e2c039b534ba052c319dc9247dd9b42
hayashi-ay <go12limchangyong@gmail.com>
2022-03-31 16:21:10 +0900

    fix: env.cppのリーク解消

--- tests/google_tests/env.cpp
+++ tests/google_tests/env.cpp
@@ -21,79 +21,88 @@ protected:
 	}
 };
 
+// actualをfreeするためのラッパー関数。actualとexpectは逆だけど仕方なし。
+void	assert_streq_wrap(char *actual, char *expect)
+{
+	ASSERT_STREQ(actual, expect);
+	free(actual);
+}
+
 TEST_F(EnvTest, normal_1)
 {
+	char	*tmp;
 	set_env("USER","takkatao");
-	ASSERT_STREQ(get_env("USER"), "takkatao");
+	assert_streq_wrap(get_env("USER"), "takkatao");
 }
 
 TEST_F(EnvTest, normal_2)
 {
 	set_env("USER","takkatao");
 	set_env("SHELL","/bin/bash");
-	ASSERT_STREQ(get_env("USER"), "takkatao");
-	ASSERT_STREQ(get_env("SHELL"), "/bin/bash");
+	assert_streq_wrap(get_env("USER"), "takkatao");
+	assert_streq_wrap(get_env("SHELL"), "/bin/bash");
 }
 
 TEST_F(EnvTest, update_1)
 {
 	set_env("HOME","/bin");
-	ASSERT_STREQ(get_env("HOME"), "/bin");
+	assert_streq_wrap(get_env("HOME"), "/bin");
 }
 
 TEST_F(EnvTest, remove_1)
 {
 	unset_env("PATH");
-	ASSERT_STREQ(get_env("HOME"), "/Users/takkatao");
-	ASSERT_STREQ(get_env("PATH"), NULL);
-	ASSERT_STREQ(get_env("PS1"), "\s-\v\$");
+	assert_streq_wrap(get_env("HOME"), "/Users/takkatao");
+	assert_streq_wrap(get_env("PATH"), NULL);
+	assert_streq_wrap(get_env("PS1"), "\s-\v\$");
 
 	unset_env("HOME");
-	ASSERT_STREQ(get_env("HOME"), NULL);
-	ASSERT_STREQ(get_env("PATH"), NULL);
-	ASSERT_STREQ(get_env("PS1"), "\s-\v\$");
+	assert_streq_wrap(get_env("HOME"), NULL);
+	assert_streq_wrap(get_env("PATH"), NULL);
+	assert_streq_wrap(get_env("PS1"), "\s-\v\$");
 
 	unset_env("PS1");
-	ASSERT_STREQ(get_env("HOME"), NULL);
-	ASSERT_STREQ(get_env("PATH"), NULL);
-	ASSERT_STREQ(get_env("PS1"), NULL);
+	assert_streq_wrap(get_env("HOME"), NULL);
+	assert_streq_wrap(get_env("PATH"), NULL);
+	assert_streq_wrap(get_env("PS1"), NULL);
 }
 
 TEST_F(EnvTest, remove_last_element)
 {
 	unset_env("PATH");
-	ASSERT_STREQ(get_env("HOME"), "/Users/takkatao");
-	ASSERT_STREQ(get_env("PATH"), NULL);
-	ASSERT_STREQ(get_env("PS1"), "\s-\v\$");
+	assert_streq_wrap(get_env("HOME"), "/Users/takkatao");
+	assert_streq_wrap(get_env("PATH"), NULL);
+	assert_streq_wrap(get_env("PS1"), "\s-\v\$");
 
 	unset_env("PS1");
-	ASSERT_STREQ(get_env("HOME"), "/Users/takkatao");
-	ASSERT_STREQ(get_env("PATH"), NULL);
-	ASSERT_STREQ(get_env("PS1"), NULL);
+	assert_streq_wrap(get_env("HOME"), "/Users/takkatao");
+	assert_streq_wrap(get_env("PATH"), NULL);
+	assert_streq_wrap(get_env("PS1"), NULL);
 }
 
 TEST_F(EnvTest, remove_all_1)
 {
 	remove_env();
-	ASSERT_STREQ(get_env("HOME"), NULL);
-	ASSERT_STREQ(get_env("PATH"), NULL);
-	ASSERT_STREQ(get_env("PS1"), NULL);
+	assert_streq_wrap(get_env("HOME"), NULL);
+	assert_streq_wrap(get_env("PATH"), NULL);
+	assert_streq_wrap(get_env("PS1"), NULL);
 }
 
 TEST_F(EnvTest, init_env_1)
 {
 	remove_env();
 	init_env();
-	ASSERT_STREQ(get_env("LANG"), std::getenv("LANG"));
+	assert_streq_wrap(get_env("LANG"), std::getenv("LANG"));
 }
 
 TEST_F(EnvTest, get_envp)
 {
 	char **envp = get_envp();
-	ASSERT_STREQ(envp[0], "HOME=/Users/takkatao");
-	ASSERT_STREQ(envp[1], "PATH=/bin");
-	ASSERT_STREQ(envp[2], "PS1=\s-\v\$");
-	ASSERT_EQ(envp[3], (char *)NULL);
+	assert_streq_wrap(envp[0], "HOME=/Users/takkatao");
+	assert_streq_wrap(envp[1], "PATH=/bin");
+	assert_streq_wrap(envp[2], "PS1=\s-\v\$");
+	ASSERT_STREQ(envp[3], (char *)NULL);
+	free(envp);
 }
 
 TEST(GetEnvKeyTest, simple)
