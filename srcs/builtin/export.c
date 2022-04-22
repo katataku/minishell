5b2c450c66bb8e7dfff294490dacc7673a754486
katataku <katataku.dev@gmail.com>
2022-04-22 19:15:25 +0900

    exportを引数なしで実行した際にenvの一覧を出力する

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -3,15 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   export.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahayashi <ahayashi@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/27 14:20:52 by takkatao          #+#    #+#             */
-/*   Updated: 2022/04/05 17:11:03 by ahayashi         ###   ########.fr       */
+/*   Updated: 2022/04/22 19:09:47 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "builtin.h"
 
+int	export_single(void)
+{
+	t_list		**env;
+	t_list		*current_env;
+	t_keyvalue	*kv;
+
+	env = get_envlist();
+	current_env = *env;
+	while (current_env != NULL)
+	{
+		kv = current_env->content;
+		if (ft_putstr_fd("declare -x ", STDOUT_FILENO) == -1
+			|| ft_putstr_fd(kv->key, STDOUT_FILENO) == -1
+			|| ft_putstr_fd("=\"", STDOUT_FILENO) == -1
+			|| ft_putstr_fd(kv->value, STDOUT_FILENO) == -1
+			|| ft_putstr_fd("\"\n", STDOUT_FILENO) == -1)
+			return (STATUS_FAILURE);
+		current_env = current_env->next;
+	}
+	return (STATUS_SUCCESS);
+}
 /*
  * usage: export
  * Mark each name to be passed to child processes in the environment.
@@ -29,6 +50,8 @@ int	builtin_export(char **argv)
 	char	*message;
 
 	return_status = STATUS_SUCCESS;
+	if (*(argv + 1) == NULL)
+		return (export_single());
 	while (*(++argv) != NULL)
 	{
 		key = ft_xstrdup(*argv);
