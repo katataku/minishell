7c1b51605ed11ad521efe198881819679bfb1eda
katataku <katataku.dev@gmail.com>
2022-04-23 16:41:11 +0900

    norm lexer.c

--- srcs/lexer/lexer.c
+++ srcs/lexer/lexer.c
@@ -6,13 +6,13 @@
 /*   By: takkatao <takkatao@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/07 12:30:12 by ahayashi          #+#    #+#             */
-/*   Updated: 2022/04/19 16:03:33 by takkatao         ###   ########.fr       */
+/*   Updated: 2022/04/23 16:40:43 by takkatao         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "lexer.h"
 
-void	tokenize_special_char(t_lexer_manager *mgr, char *str)
+static int	tokenize_special_char(t_lexer_manager *mgr, char *str)
 {
 	if (*str == '|')
 		mgr->token->token[mgr->token_index++] = T_BAR;
@@ -24,6 +24,10 @@ void	tokenize_special_char(t_lexer_manager *mgr, char *str)
 		mgr->token->token[mgr->token_index++] = T_LTLT;
 	if (*str == '<' && *(str + 1) != '<')
 		mgr->token->token[mgr->token_index++] = T_LT;
+	if (mgr->token->token[mgr->token_index - 1] == T_GTGT
+		|| mgr->token->token[mgr->token_index - 1] == T_LTLT)
+		return (1);
+	return (0);
 }
 
 int	lexer_neutral(t_lexer_manager *mgr, char *str)
@@ -32,28 +36,17 @@ int	lexer_neutral(t_lexer_manager *mgr, char *str)
 
 	init_str = str;
 	if (is_special_char(*str))
+		return (tokenize_special_char(mgr, str) + 1);
+	while (!is_special_char(*str))
 	{
-		tokenize_special_char(mgr, str);
-		if (mgr->token->token[mgr->token_index - 1] == T_GTGT
-			|| mgr->token->token[mgr->token_index - 1] == T_LTLT)
-			str++;
-		str++;
+		set_state(mgr, str);
+		mgr->word[mgr->word_index++] = *str++;
+		if (mgr->state != NEUTRAL)
+			return (str - init_str);
 	}
-	else
-	{
-		while (!is_special_char(*str))
-		{
-			set_state(mgr, str);
-			mgr->word[mgr->word_index++] = *str++;
-			if (mgr->state != NEUTRAL)
-			{
-				return (str - init_str);
-			}
-		}
-		mgr->word[mgr->word_index] = '\0';
-		set_token(mgr->token, mgr->token_index++, T_WORD, mgr->word);
-		mgr->word_index = 0;
-	}
+	mgr->word[mgr->word_index] = '\0';
+	set_token(mgr->token, mgr->token_index++, T_WORD, mgr->word);
+	mgr->word_index = 0;
 	return (str - init_str);
 }
 
@@ -76,7 +69,7 @@ int	lexer_not_neutral(t_lexer_manager *mgr, char *str)
 			mgr->word[mgr->word_index++] = *str++;
 	if (*str == '\0')
 		mgr->is_misuse_builtin = true;
-	if (*str != '\0')
+	else
 		mgr->word[mgr->word_index++] = *str++;
 	if (is_special_char(*str))
 	{
@@ -103,24 +96,8 @@ void	free_lexer_token(t_token *token)
 	free(token);
 }
 
-t_token	*lexer(char *str)
+static void	lexer_postprocess(t_lexer_manager	*mgr)
 {
-	t_lexer_manager	*mgr;
-	t_token			*token;
-
-	mgr = initialize_lexer(str);
-	mgr->word_index = 0;
-	while (*str != '\0')
-	{
-		while ((*str == ' ' || *str == '\t') && mgr->state == NEUTRAL)
-			str++;
-		if (*str == '\0')
-			break ;
-		if (mgr->state == NEUTRAL)
-			str += lexer_neutral(mgr, str);
-		else
-			str += lexer_not_neutral(mgr, str);
-	}
 	if (mgr->is_misuse_builtin || mgr->state != NEUTRAL)
 	{
 		g_last_exit_status = STATUS_MISUSE_BUILTIN;
@@ -133,6 +110,26 @@ t_token	*lexer(char *str)
 		free_lexer_token(mgr->token);
 		mgr->token = NULL;
 	}
+}
+
+t_token	*lexer(char *str)
+{
+	t_lexer_manager	*mgr;
+	t_token			*token;
+
+	mgr = initialize_lexer(str);
+	while (*str != '\0')
+	{
+		while ((*str == ' ' || *str == '\t') && mgr->state == NEUTRAL)
+			str++;
+		if (*str == '\0')
+			break ;
+		if (mgr->state == NEUTRAL)
+			str += lexer_neutral(mgr, str);
+		else
+			str += lexer_not_neutral(mgr, str);
+	}
+	lexer_postprocess(mgr);
 	token = mgr->token;
 	free(mgr);
 	return (token);
